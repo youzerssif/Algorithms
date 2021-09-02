3efe830b596147c77b944ad8bae1bebf15332e97
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-09-02 16:31:13 +0000

    Study class java

--- java/hello.java
+++ java/hello.java
@@ -1,28 +1,39 @@
-import java.security.*;
-import java.util.Scanner;
+// import java.security.*;
+// import java.util.Scanner;
+
 
 // Your First Program
 
 class Hello {
+
+    String ma_cahine;
+    Hello(String ma_cahine){
+        this.ma_cahine = ma_cahine;
+        System.out.println("Welcome Constructor"+ this);
+    }    
     public static void main(String[] args) {
         int addt = Addition.add();
-        System.out.println("Hello, World !"); 
+        // ma_classe.add();
+        System.out.println("Hello"+ addt); 
+         
     }
 }
 
 class Addition{
 
     public static int add(){
-        Scanner scanner = new Scanner(System.in);
-        int cmpt = 1;
-        // int a = scanner.nextInt();
-        int nombre = scanner.nextInt();
-        while (cmpt < 11) {
-            System.out.println(nombre +" X "+ cmpt +"="+ nombre*cmpt);
-            cmpt++;
+        Object ma_classe = new Hello("Bonjour");
+        System.out.println("Constructor spell! "+ ma_classe);
+        int[] numbers = {3, 9, 5, -5};
+        // System.out.println(numbers[0]);
+        int sum = 0;
+        for( int numb : numbers){
+            sum += numb;
+
         }
+        // System.out.println("Ok on affiche "+ sum);
         
-        return 0;
+        return sum;
     }
     
 }
